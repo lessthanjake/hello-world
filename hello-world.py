befbf0fc5f5a47d9bccc94f29807dea7f3bfbb47
lessthanjake <jacobbharris@gmail.com>
2016-10-18 15:01:56 -0600

    Update hello-world.py
    Removed a line.

--- hello-world.py
+++ hello-world.py
@@ -2,5 +2,3 @@ print("Hello, World!")
 print("This is a test for Branch-test 1")
 #print("I hope all goes well")
 ## commented out the above line on remote mac station
-## added another line to comment out here.
-
