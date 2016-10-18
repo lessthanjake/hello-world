26a234279133b243efb06962c90d71f92845be2c
jacobbharris <jacobbharris@gmail.com>
2016-10-18 14:56:52 -0600

    Commented out on remote branch.

--- hello-world.py
+++ hello-world.py
@@ -1,3 +1,5 @@
 print("Hello, World!")
 print("This is a test for Branch-test 1")
-print("I hope all goes well")
+#print("I hope all goes well")
+## commented out the above line on remote mac station
+
