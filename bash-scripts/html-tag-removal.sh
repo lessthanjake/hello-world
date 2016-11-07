98508ea1609a17c842b8c2e5769de03658c9c167
jacobbharris <jacobbharris@gmail.com>
2016-11-07 13:18:43 -0700

    Added a bash script that removes html tags from .txt files within a directory.

---
+++ bash-scripts/html-tag-removal.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# This script crawls the current directory of .txt files and strips out all the HTML tags < >
+# The new files are written into a folder called stripped
+echo "Current directory is "
+pwd
+read -r -p "Continue? [Y/n]" response
+response=${response}
+if [[ $response =~ ^(yes|y|Y) ]]; then
+	mkdir stripped
+	for file in ./*.txt
+		do
+			name=${file##*/}
+			base=${name%.txt}
+			cat "$file" |sed 's/<[^>]*>//g' > ./stripped/"${base}.txt"
+		done
+else
+	echo "Not run, Exiting"
+fi
