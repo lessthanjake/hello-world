d5fd750b1ad7845faf9f5d9611bff79fdc78d2de
jacobbharris <jacobbharris@gmail.com>
2016-11-07 19:45:43 -0700

    Removed some directory string things that didn't work...

--- bash-scripts/html-tag-removal.sh
+++ bash-scripts/html-tag-removal.sh
@@ -5,14 +5,14 @@ echo "Current directory is "
 pwd
 read -r -p "Continue? [Y/n]" response
 response=${response}
-if [[ $response =~ ^(yes|y|Y) ]]; then
-	mkdir stripped
-	for file in ./*.txt
-		do
-			name=${file##*/}
-			base=${name%.txt}
-			cat "$file" |sed 's/<[^>]*>//g' > ./stripped/"${base}.txt"
-		done
+if [[ ${response} =~ ^(yes|y|Y) ]]; then
+        mkdir stripped
+        for file in ./*.txt
+                do
+                        name=${file##*/}
+                        base=${name%.txt}
+                        cat "$file" |sed 's/<[^>]*>//g' > ./stripped/"${base}.txt"
+                done
 else
-	echo "Not run, Exiting"
+        echo "Not run, Exiting"
 fi
